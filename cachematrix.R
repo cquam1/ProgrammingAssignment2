34f787a26743a7a8d45a286d460341f42e10e4b3
cquam1 <cquam1@gmail.com>
2018-03-08 16:31:03 -0800

    cache rules everything around me

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## make matrix object to store inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  # initialize the inverse
+  
+  inv <- NULL
+  # setter not necessary 
+  get <- function() x
+  setinv <- function(matinv) inv <<- matinv
+  getinv <- function() inv
+  
+  list(get = get, setinv = setinv, getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## pull inverse from cache, or calculate then cache inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinv()
+  if(!is.null(inv))
+  {
+    message("cache rules everything around me")
+    return(inv)
+    
+  }
+  
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  message("depositing some cache, see what i did there")
+  inv
+  
+  
 }
